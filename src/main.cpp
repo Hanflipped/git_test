d52243b8de180de22499fe896c61ffc8080ade07
Hanflipped <hanflipped@gmail.com>
2022-08-02 23:58:50 +0800

    2358

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Why so hard!" << std::endl;
+	std::cout << "Hello TaiWan" << std::endl;
+	std::cout << "why???" << std::endl;
+}
