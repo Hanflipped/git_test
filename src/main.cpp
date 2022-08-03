02f825336ba1203df82bd4f0b22553ce0978dbc7
Hanflipped <hanflipped@gmail.com>
2022-08-03 20:34:54 +0800

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -5,4 +5,6 @@ int main(int argc, char *argv[])
 	std::cout << "Why so hard!" << std::endl;
 	std::cout << "Hello TaiWan" << std::endl;
 	std::cout << "why???" << std::endl;
+	std::cout << "she???" << std::endl;
+	std::cout << "20:32" << std::endl;
 }
