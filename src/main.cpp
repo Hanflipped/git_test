3c141344bccdaf51bc3d1ebf765d6dad2a4c4aee
Hanfliipped <hanflipped@gmail.com>
2022-09-02 20:49:14 +0800

    base版本

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,7 @@ int main(int argc, char *argv[])
 	std::cout << "why???" << std::endl;
 	std::cout << "she???" << std::endl;
 	std::cout << "20:32" << std::endl;
+
+	std::cout << "测试diff" << std::endl;
+	std::cout << "对比项目" << std::endl;
 }
